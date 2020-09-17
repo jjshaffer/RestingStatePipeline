5688a47eb2bdd90ee0ddebab9d13130818669d27
Joe Shaffer <jjshafferjr@gmail.com>
2020-09-17 14:34:39 -0500

    Update getSubjectIndex.sh

--- getSubjectIndex.sh
+++ getSubjectIndex.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
-#i="$1"
+#Script for looping through all subjects in the data directory in order to identify their index number in case it is necessary to run resting-state analysis on a sub-set.
+#There is a chance that the provided index may be off by one, depending on whether you're running this locally or using the cluster. Keep this in mind and double check when
+# running the "readBids" script. 
+#Author: Joe Shaffer
+#Date December, 2019
+
+BIDS_DIR="$1"
 
 #Cluster Path
 #SHARE_DIR=/Shared/MRRCdata
-
 #Local Path
-SHARE_DIR=/Volumes/mrrcdata
-
-
-BIDS_DIR=${SHARE_DIR}/Bipolar_R01/BD_R01_data
-FREESURFER_DIR=${SHARE_DIR}/Bipolar_R01/derivatives/FreeSurfer
-
-
+#SHARE_DIR=/Volumes/mrrcdata
+#BIDS_DIR=${SHARE_DIR}/Bipolar_R01/BD_R01_data
 
 #Find all subjects in BIDS_DIR & sessions
 subjects=(${BIDS_DIR}/*/)
 numsubjs=${#subjects[@]}
 #echo $numsubjs
 
-
 #Loop through each subject
 for i in `seq 0 $(($numsubjs-1))`
-#for i in `seq 0 0`
 do
 
-#echo ${subjects[$i]}
-subject=${subjects[$i]}
+  #echo ${subjects[$i]}
+  subject=${subjects[$i]}
 
-#Trim / at end
-subject=${subject%*/}
-#echo $subject
+  #Trim / at end
+  subject=${subject%*/}
+  #echo $subject
 
-#Trim base file path
-subject=${subject##*/}
-#echo $subject
+  #Trim base file path
+  subject=${subject##*/}
+  #echo $subject
 
-if [[ $subject != "sourcedata" ]]; then
+  if [[ $subject != "sourcedata" ]]; then
 
-#Trim everything preceding -
-subject=${subject#*-}
+    #Trim everything preceding -
+    subject=${subject#*-}
 
-echo ${i}:${subject}
-fi
+    echo ${i}:${subject}
+  fi
 
 done #subject
